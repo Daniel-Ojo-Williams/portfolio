82ab2be608933e8b36a2ed05237e6658a177d3ee
Ojo-williams Daniel <ojowilliamsdaniel@gmail.com>
2023-12-01 12:37:17 +0100

    fixed liveLink not showing

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,7 +5,7 @@ const projects = [
     description:
       "Condenses long URLs into shorter, more manageable links, redirecting users to the original URL when accessed.",
     linkCode: "https://github.com/Daniel-Ojo-Williams/urlShortener",
-    linkLive: "minify.onrender.com"
+    linkLive: "https://minify.onrender.com",
   },
 ];
 
